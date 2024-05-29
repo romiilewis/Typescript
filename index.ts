d79c1a7ab4294a8edf7bf820ee820db8c86c021f
Romi <romiilewis@gmail.com>
2024-05-29 20:16:23 +0100

    Adding variables

--- index.ts
+++ index.ts
@@ -21,14 +21,35 @@
 
 
 
-type NumberAndStringTupleType = [number, string]
-let tupleVariable: NumberAndStringTupleType;
-tupleVariable = [1, "goodbye"]
+// type NumberAndStringTupleType = [number, string]
+// let tupleVariable: NumberAndStringTupleType;
+// tupleVariable = [1, "goodbye"]
 
 
 
-type Triple = [number, string, string[]]
-let tripleVirable: Triple;
-tripleVirable = [1, "whoop", ["first, second, third"]]
+// type Triple = [number, string, string[]]
+// let tripleVirable: Triple;
+// tripleVirable = [1, "whoop", ["first, second, third"]]
 
 
+
+function add(a:number, b:number){
+    return a + b
+}
+
+const addLambda = (a:number, b:number) => a + b
+
+const sum = add(3,4)
+const sumLambda = addLambda (3,4)
+
+
+
+const multiply = (a:number, b?:number) => {
+    if (b) {
+        return a * b 
+    }
+
+  return a * 2  
+}
+
+console.log(multiply(3))
